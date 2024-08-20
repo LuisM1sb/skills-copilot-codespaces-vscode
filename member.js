2e753aededf5651d296ebf9c7d0760478b98653f
Luis Méndez <163043415+LuisM1sb@users.noreply.github.com>
2024-08-20 01:19:14 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        details: function () {
+            console.log(this.name + ' has ' + this.skills.length + ' skills, and they are ' + this.skills);
+        }
+    };
+    return member;
+}
+
+
